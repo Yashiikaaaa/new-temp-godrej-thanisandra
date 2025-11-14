a187e9ce84157ab5fc485474ce7e16df37acef59
yashiikaaaa <yashika@truestate.in>
2025-11-14 18:56:52 +0530

    googlr tagg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N2F9R3DQ');`,
+})(window,document,'script','dataLayer','GTM-TN2TJW7V');`,
           }}
         />
         <script
@@ -43,7 +43,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 window.dataLayer = window.dataLayer || [];
 function gtag(){ window.dataLayer.push(arguments); }
 gtag('js', new Date());
-gtag('config', 'G-3J8R39GCCX');
+gtag('config', 'G-TCMEYMB0TZ');
 `,
           }}
         />
